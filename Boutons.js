e9b51d7c9ce7198246c124c81dd0a7d09d2b49ef
Lepionf6 <cpderegnaucourt@sfr.fr>
2026-08-17 17:17:44 +0200

    Add files via upload

---
+++ Boutons.js
@@ -0,0 +1,34 @@
+// chargement des boutons
+
+var bouton1 = new Array();var bouton2 = new Array();
+
+for (var i = 0; i<7; i++ ) {bouton1[i] = new Image();}
+bouton1[0].src = "Boutons/presentation1.jpg";
+bouton1[1].src = "Boutons/tournois1.jpg";
+bouton1[2].src = "Boutons/evenements1.jpg";
+bouton1[3].src = "Boutons/liens1.jpg";
+bouton1[4].src = "Boutons/tarifs1.jpg";
+bouton1[5].src = "Boutons/archives1.jpg";
+bouton1[6].src = "Boutons/mentions1.jpg";
+
+
+for (var i = 0; i<7; i++ ) {bouton2[i] = new Image();}
+bouton2[0].src = "Boutons/presentation2.jpg";
+bouton2[1].src = "Boutons/tournois2.jpg";
+bouton2[2].src = "Boutons/evenements2.jpg";
+bouton2[3].src = "Boutons/liens2.jpg";
+bouton2[4].src = "Boutons/tarifs2.jpg";
+bouton2[5].src = "Boutons/archives2.jpg";
+bouton2[6].src = "Boutons/mentions2.jpg";
+
+function changeBouton1(numeroBouton){
+	n=numeroBouton+1;
+	n0=numeroBouton-1;
+	document.images[n].src=bouton1[n0].src;
+}
+function changeBouton2(numeroBouton){
+	n=numeroBouton+1;
+	n0=numeroBouton-1;
+	document.images[n].src=bouton2[n0].src;
+}
+
