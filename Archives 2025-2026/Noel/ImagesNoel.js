1cfc897392555de83f16242961c46c6a272592af
Lepionf6 <cpderegnaucourt@sfr.fr>
2026-08-23 19:39:21 +0200

    Add files via upload

--- Archives 2025-2026/Noel/ImagesNoel.js
+++ Archives 2025-2026/Noel/ImagesNoel.js
@@ -12,21 +12,62 @@ image2[1].src = "Noel/Noel 2.jpg";
 image2[2].src = "Noel/Resultats noel.jpg";
 
 //duree de changement d'images
-delaiImage = 4.;var numeroImage = 1;
+delaiImage1 = 4.;var numeroImage1 = 1;
+delaiImage2 = 4.;var numeroImage2 = 1;
+
+// variables de pause
+var pauseImage1 = false;
+var pauseImage2 = false;
 
 //changement des photos des événements
-function changePhoto(photo){
+function changePhoto1(photo){
 	document.images[15].src = image1[photo].src;
+}
+
+function incrementationImage1() {
+	if (!pauseImage1) {
+		changePhoto1(numeroImage1);
+		numeroImage1++;
+		if (numeroImage1 == 3) {
+			numeroImage1 = 0;
+		}
+	}
+	setTimeout("incrementationImage1()",delaiImage1*1000);
+}
+
+function changePhoto2(photo){
         document.images[16].src = image2[photo].src;
 }
 
-function incrementationImage() {
-	changePhoto(numeroImage);
-	numeroImage++;
-	if (numeroImage == 3) {
-		numeroImage = 0;
+function incrementationImage2() {
+	if (!pauseImage2) {
+		changePhoto2(numeroImage2);
+		numeroImage2++;
+		if (numeroImage2 == 3) {
+			numeroImage2 = 0;
+		}	
 	}
-	setTimeout("incrementationImage()",delaiImage*1000);
+	setTimeout("incrementationImage2()",delaiImage2*1000);
 }
-setTimeout("incrementationImage()",delaiImage*1000);
+
+// Pause / reprise au passage de la souris
+document.images[15].onmouseover = function() {
+    pauseImage1 = true;
+};
+
+document.images[15].onmouseout = function() {
+    pauseImage1 = false;
+};
+
+document.images[16].onmouseover = function() {
+    pauseImage2 = true;
+};
+
+document.images[16].onmouseout = function() {
+    pauseImage2 = false;
+};
+
+// lancement des diaporamas
+setTimeout("incrementationImage1()",delaiImage1*1000);
+setTimeout("incrementationImage2()",delaiImage2*1000);
 
