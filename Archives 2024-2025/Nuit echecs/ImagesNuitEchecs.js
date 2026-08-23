3a4872ceeb54e457725e20d614e1e7d103f13411
Lepionf6 <cpderegnaucourt@sfr.fr>
2026-08-23 19:34:26 +0200

    Add files via upload

--- Archives 2024-2025/Nuit echecs/ImagesNuitEchecs.js
+++ Archives 2024-2025/Nuit echecs/ImagesNuitEchecs.js
@@ -16,36 +16,62 @@ image2[3].src = "Nuit echecs/Nuit des echecs 6.jpg";
 image2[4].src = "Nuit echecs/Nuit des echecs 5.jpg";
 
 //duree de changement d'images
-delaiImage = 4.;var numeroImage = 1;
+delaiImage1 = 4.;var numeroImage1 = 1;
 delaiImage2 = 4.;var numeroImage2 = 1;
 
+// variables de pause
+var pauseImage1 = false;
+var pauseImage2 = false;
+
 //changement des photos des événements
-function changePhoto(photo){
+function changePhoto1(photo){
 	document.images[12].src = image1[photo].src;
 }
 
-function incrementationImage() {
-	changePhoto(numeroImage);
-	numeroImage++;
-	if (numeroImage == 5) {
-		numeroImage = 0;
+function incrementationImage1() {
+	if (!pauseImage1) {
+		changePhoto1(numeroImage1);
+		numeroImage1++;
+		if (numeroImage1 == 5) {
+			numeroImage1 = 0;
+		}
 	}
-	setTimeout("incrementationImage()",delaiImage*1000);
+	setTimeout("incrementationImage1()",delaiImage1*1000);
 }
 
-function changePhoto2(photo2){
-        document.images[13].src = image2[photo2].src;
+function changePhoto2(photo){
+        document.images[13].src = image2[photo].src;
 }
 
 function incrementationImage2() {
-	changePhoto2(numeroImage2);
-	numeroImage2++;
-	if (numeroImage2 == 5) {
-		numeroImage2 = 0;
+	if (!pauseImage2) {
+		changePhoto2(numeroImage2);
+		numeroImage2++;
+		if (numeroImage2 == 5) {
+			numeroImage2 = 0;
+		}	
 	}
 	setTimeout("incrementationImage2()",delaiImage2*1000);
 }
 
-setTimeout("incrementationImage()",delaiImage*1000);
+// Pause / reprise au passage de la souris
+document.images[12].onmouseover = function() {
+    pauseImage1 = true;
+};
+
+document.images[12].onmouseout = function() {
+    pauseImage1 = false;
+};
+
+document.images[13].onmouseover = function() {
+    pauseImage2 = true;
+};
+
+document.images[13].onmouseout = function() {
+    pauseImage2 = false;
+};
+
+// lancement des diaporamas
+setTimeout("incrementationImage1()",delaiImage1*1000);
 setTimeout("incrementationImage2()",delaiImage2*1000);
 
